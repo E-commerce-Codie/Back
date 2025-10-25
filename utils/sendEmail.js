474ca4f011bc70934e75c9eed1cb7a20204679a4
Mahmoud-Badawy1 <mahmoudbadawy10h@gmail.com>
2025-10-25 16:12:31 +0300

    update sendEmail

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,22 +1,44 @@
-const NodeMailer = require("nodemailer");
+// const NodeMailer = require("nodemailer");
+
+// const sendEmails = async (options) => {
+//   const transporter = NodeMailer.createTransport({
+//     host: process.env.Email_HOST,
+//     port: process.env.Email_PORT,
+//     secure: process.env.Email_SECURE,
+//     auth: {
+//       user: process.env.EMAIL_USER,
+//       pass: process.env.EMAIL_PASSWORD,
+//     },
+//   });
+//   const mailOptions = {
+//     from: process.env.EMAIL_FROM,
+//     to: options.email,
+//     subject: options.subject,
+//     text: options.message,
+//   };
+//   await transporter.sendMail(mailOptions);
+// };
+
+// module.exports = sendEmails;
+// normal way for backend real not Vercel or Netlify serverless functions
+
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmails = async (options) => {
-  const transporter = NodeMailer.createTransport({
-    host: process.env.Email_HOST,
-    port: process.env.Email_PORT,
-    secure: process.env.Email_SECURE,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  const mailOptions = {
-    from: process.env.EMAIL_FROM,
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
-  await transporter.sendMail(mailOptions);
+  try {
+    const data = await resend.emails.send({
+      from: process.env.EMAIL_FROM, // Example: 'Codie <onboarding@resend.dev>'
+      to: options.email,            // e.g. "user@example.com"
+      subject: options.subject,
+      text: options.message,
+    });
+
+    console.log('Email sent successfully:', data);
+  } catch (error) {
+    console.error('Error sending email:', error);
+  }
 };
 
-module.exports = sendEmails;
+export default sendEmails;
